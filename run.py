de01cedc92bd74d4c208522a57dec2208489374a
Destan Tezel <destantezel@googlemail.com>
2023-05-23 22:06:48 +0000

    Create record_income_data function that returns str from user

--- run.py
+++ run.py
@@ -13,3 +13,28 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('small_business_books')
+
+
+def record_income_data(year):
+    """
+    Get income data input from the user.
+    """
+    headings_string = ''
+    headings_list = SHEET.worksheet(f'income_{year}').row_values(1)
+
+    for heading in headings_list[0:-3]:
+        headings_string += heading + ', '
+    headings_string += headings_list[-3]
+
+    print('Enter the date and income data, separated by commas.')
+    print('Data should be in the corresponding order:\n')
+    print(headings_string)
+    print('Example: DD/MM/YYYY,500,600,700,800,500,400\n')
+
+    data_str = input('Enter your data here:\n')
+
+    income_data = data_str.split(',')
+
+    return income_data
+    
+record_income_data(2023)
